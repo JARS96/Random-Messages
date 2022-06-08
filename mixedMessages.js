758dc42782a786d4c1a311988a26c5cda5332584
JARS <J.ARS96@outlook.com>
2022-06-07 19:37:50 -0500

    Random greetings

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,7 +3,7 @@ console.log('Hello, World!')
 */
 
 //starting code
-const firstPartGreet = ['Hi', 'Hello', 'Hallo', 'Hola', 'Salut', 'Ciao'];
+const firstPartGreet = ['Hi', 'Hello', 'Hallo', 'Hola', 'Salut', 'Ciao', 'Olá','Privet','Nǐ hǎo','Witam','Dia dhuit'];
 let objects = [
     {
         name: 'King Carlo Magno',
@@ -25,6 +25,30 @@ let objects = [
         name:'Lucio',
         placeCome:'Rome'
     },
+    {
+        name: 'Darwin',
+        placeCome: 'United kingdom',
+    },
+    {
+        name: 'Platón',
+        placeCome: 'Atenas',
+    },
+    {
+        name: 'Einstein',
+        placeCome: 'Germany',
+    },
+    {
+        name: 'Newton',
+        placeCome: 'Manor',
+    },
+    {
+        name: 'Muhammad',
+        placeCome: 'United States',
+    },
+    {
+        name: 'Tesla',
+        placeCome: 'Croatia',
+    },
 ]
 var num = Math. round(Math. random()*10);
 
@@ -35,38 +59,23 @@ var num = Math. round(Math. random()*10);
 const randomMessage = () =>{
     let i = Math. round(Math. random()*10);
     let j = Math. round(Math. random()*10);
-    switch(num){
-        case 0:
-            console.log(objects[i].name[j])
-        case 1:
-            console.log(num);
-        case 2:
-            console.log(num);
-        case 3:
-            console.log(num);
-        case 4:
-            console.log(num);
-        case 5:
-            console.log(num);
-        case 6:
-            console.log(num);
-        case 7:
-            console.log(num);
-        case 8:
-            console.log(num);
-        case 9:
-            console.log(num);
-        case 10:
-            console.log(num);
-    }
-
+    let k = Math. round(Math. random()*100);
+    let message = [];
+    message.push(firstPartGreet[j])
+    message.push(objects[i].name)
+  return message.join(' ') +', ' + ' ' + `We have been waiting for you since ${k} years ago.`;
 }
 
 
 //Testing codes
-console.log(objects[1].name)
-console.log(num);
+//console.log(objects[1].name)
+//console.log('primer separador');
+//console.log(num);
+//console.log('segundo separador');
 console.log(randomMessage());
-console.log(objects[i].name[j])
+//console.log('tercer separador');
+//console.log(objects[i].name);
+//console.log('fin');
+//randomMessage();
 
 
